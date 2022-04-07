60e64462f7f333d694ce6d97ff6e2953de81219e
Haris Pasic <harispasic@ymail.com>
2022-04-07 21:16:06 +0200

    Modifiying data with array

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,12 @@
+var myArr = [2, 4, 0, 8, 10];
 
+function myArrayFunction(myItems){
+var myItems = [2];
+myArr[myItems] = 6;
+
+console.log(myArr)
+}
+
+myArrayFunction();
+
+module.exports = myArrayFunction;
