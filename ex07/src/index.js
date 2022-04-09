62795675f1c473798a6ed7b44597974d82b268d3
Haris Pasic <harispasic@ymail.com>
2022-04-09 15:38:12 +0200

    Nesting for loop

---
+++ ex07/src/index.js
@@ -0,0 +1,25 @@
+var myArray = [
+    [[2],[5,6,7],[8,9]],
+    [[2.5, 2],[0.5, 0.2],[8]],
+    [[1,2],[3,4,5,6],[7,8,9]]
+]
+
+function multiplyArrayFunction(myArray){
+    var product = 1;
+    var sum = 0;
+   
+   for(var i=0; i < myArray.length; i++){
+       for(var j=0; j < myArray[i].length; j++) {
+           product = product * myArray[i][j];
+           sum = sum + myArray[i][j];
+           }
+   }
+   return {product,
+sum};
+   }
+   console.log(multiplyArrayFunction([[2],[5,6,7],[8,9]]));
+   console.log(multiplyArrayFunction([[2.5, 2],[0.5, 0.2],[8]]));
+   console.log(multiplyArrayFunction([[1,2],[3,4,5,6],[7,8,9]]));
+   
+   module.exports = multiplyArrayFunction;
+   
