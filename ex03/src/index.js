9a2137dbdb0788aaf735e500f5b559f1fed9210b
Haris Pasic <harispasic@ymail.com>
2022-04-09 14:37:08 +0200

    Manipulate Arrays

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -7,12 +7,13 @@ var firstPet = myNewPets[0];
 console.log(firstPet);
 var lastPet = myNewPets[1];
 console.log(lastPet);
-myNewPets.push("Lion");
+myNewPets.pop();
 myNewPets.shift();
-myNewPets.shift();
+myNewPets.unshift("Lion");
 myPetsArray = myNewPets.push("Cat");
 myNewPets.push("Bird");
 console.log(myNewPets);
+return myNewPets;
 }
 
 myArrayFunction();
