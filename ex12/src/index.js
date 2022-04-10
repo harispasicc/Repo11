7c1f8f2cf068b0a9839aa70592c0d597973a0ee5
Haris Pasic <harispasic@ymail.com>
2022-04-10 12:43:28 +0200

    Splitting array

---
+++ ex12/src/index.js
@@ -0,0 +1,17 @@
+function splitArrayInGroups(arr ,n) {
+    var result = [];
+    while (arr.length > n) {
+        result.push(arr.splice(0, n))
+    }
+    if (arr.length) {
+        result.push(arr);
+    }
+    return result;
+
+}
+
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
